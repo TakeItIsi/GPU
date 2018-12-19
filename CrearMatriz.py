cb31d428d03fdb8a25ac44eb64ac9967038865eb
TakeItIsi <isidora.chuo@hotmail.cl>
2018-12-19 19:29:11 -0300

    MATRICES ARE READY

--- CrearMatriz.py
+++ CrearMatriz.py
@@ -5,23 +5,19 @@ import numpy as np
 #entre pais y continente
 def NuevaMatriz(conti, pais, cp):
     mat=np.zeros((len(conti), len(pais)))
-    for p in pais:
-        #print(p)
+    p=0
+    while(p<len(pais)):
         i=0
         while(i<len(conti)):
-            #print(conti[i])
-            #print([p[1], conti[i][1]])
-            if [p[1], conti[i][1]] in cp:
-                mat[i][p[0]]= 1
-                #print("ye")
-            #else:
-                #print("nah")
+            if cp[pais[str(p)]]==conti[str(i)]:
+                    mat[i][p]= 1
             i=i+1
+        p=p+1
     return mat
 
 #Test
 
-ccc = [[0,"a"],[1,"b"],[2,"c"]]
-ppp = [[0,"alfa"],[1,"beta"],[2,"alfalfa"],[3,"comino"],[4,"cuchifli"],[5,"bumblebee"]]
-cyp =[["alfa","a"],["alfalfa","a"],["beta","b"],["bumblebee","b"],["comino","c"],["cuchifli","c"]]
-print(NuevaMatriz(ccc,ppp,cyp))
+#ccc = {"0":"a","1":"b","2":"c"}
+#ppp = {"0":"alfa","1":"beta","2":"alfalfa","3":"comino","4":"cuchifli","5":"bumblebee"}
+#cyp =[["alfa","a"],["alfalfa","a"],["beta","b"],["bumblebee","b"],["comino","c"],["cuchifli","c"]]
+#print(NuevaMatriz(ccc,ppp,cyp))
--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import pycuda
 from CrearMatriz import NuevaMatriz
 import json
+#from Tablas import continentes, paises, ciudades
+import pandas
+import numpy as np
 
 
 
-f= open("probando.txt","w+")
-ciudades = json.load("cities.json")
-paises = json.load("countries.json")
-continentes = json.load("continents.json")
-for a in ciudades:
+with open("countries.json") as f:
+    paises = json.load(f)
 
-#CrearMatriz(continentes, paises, )
+with open("cities.json") as f:
+    ciudades = json.load(f)
+
+with open("continents.json") as f:
+    continentes = json.load(f)
+
+paiscont = pandas.read_csv("countries-continent.csv", sep=';')
+paiscontdic = dict(zip(paiscont.Country, paiscont.Continent))
+
+ciupai = pandas.read_csv("cities-country.csv", sep=";")
+ciupaidic = dict(zip(ciupai.name, ciupai.country))
+
+paiscontinente = NuevaMatriz(continentes, paises, paiscontdic)
+ciudadpais = NuevaMatriz(paises, ciudades, ciupaidic)
+
+np.savetxt('PaisEnContinente.txt', paiscontinente, fmt='%.1i')
+np.savetxt('CiudadEnPais.txt', ciudadpais, fmt='%.1i')
+
+
